3aa268d48bf50c0cb16d32fe516d40b726586ac4
tkr466 <lena.stridsberg@teliacompany.com>
2017-02-24 10:53:34 +0100

    Refactor and change mocks for challengetest

--- src/test/com/challengeaccepted/controllers/ChallengeControllerTest.java
+++ src/test/com/challengeaccepted/controllers/ChallengeControllerTest.java
@@ -7,71 +7,73 @@ import com.challengeaccepted.services.UserService;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.springframework.http.HttpStatus;
 
 import java.util.ArrayList;
 import java.util.Random;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class ChallengeControllerTest {
 
-    @Mock
-    private ChallengeService mockedChallengeService;
-    @Mock
-    private UserService mockedUserService;
-    @Mock
-    private Challenge mockedChallenge;
-    @Mock
-    private User mockedUser;
-    @Mock
-    private User nullMockedUser = null;
-    @Mock
-    private ArrayList<Long> mockedUserModelList;
-    @Mock
-    private User mockedChallengeCreator;
+    private ChallengeService challengeService;
+    private UserService userService;
+    private Challenge challenge;
+    private User user;
+    private ArrayList<Long> userList;
+    private User challengeCreator;
 
     @InjectMocks
     private ChallengeController challengeController;
 
     @Before
     public void setUp() throws Exception {
-        MockitoAnnotations.initMocks(this);
-        when(mockedChallengeService.getChallengeFromDatabase(1L)).thenReturn(mockedChallenge);
-        when(mockedUserService.getUserFromDatabase(1L)).thenReturn(mockedUser);
-        when(mockedChallenge.getChallengeUpvoters()).thenReturn(mockedUserModelList);
-        when(mockedUserService.getUserFromDatabase(mockedUser.getId())).thenReturn(mockedUser);
-        when(mockedChallenge.getChallengeClaimer()).thenReturn(mockedUser);
-        when(mockedChallenge.getChallengeCreator()).thenReturn(mockedChallengeCreator);
+        challengeService = mock(ChallengeService.class);
+        userService = mock(UserService.class);
+        challenge = mock(Challenge.class);
+        user = mock(User.class);
+        userList = new ArrayList<>();
+        challengeCreator = mock(User.class);
+
+        when(challengeService.getChallengeFromDatabase(1L)).thenReturn(challenge);
+        when(userService.getUserFromDatabase(1L)).thenReturn(user);
+        when(challenge.getChallengeUpvoters()).thenReturn(userList);
+        when(userService.getUserFromDatabase(user.getId())).thenReturn(user);
+        when(challenge.getChallengeClaimer()).thenReturn(user);
+        when(challenge.getChallengeCreator()).thenReturn(challengeCreator);
     }
 
     @Test
     public void testCreateChallenge() throws Exception {
-        when(mockedChallenge.getTopic()).thenReturn("");
-        when(mockedChallenge.getDescription()).thenReturn("");
-        assertEquals(HttpStatus.NO_CONTENT, challengeController.createChallenge(mockedChallenge, new Random().nextLong()).getStatusCode());
-        when(mockedChallenge.getTopic()).thenReturn("Topic");
-        when(mockedChallenge.getDescription()).thenReturn("Description");
-        assertEquals(HttpStatus.CREATED, challengeController.createChallenge(mockedChallenge, new Random().nextLong()).getStatusCode());
+        when(challenge.getTopic()).thenReturn("");
+        when(challenge.getDescription()).thenReturn("");
+
+        assertEquals(HttpStatus.NO_CONTENT, challengeController.createChallenge(challenge, new Random().nextLong()).getStatusCode());
+
+        when(challenge.getTopic()).thenReturn("Topic");
+        when(challenge.getDescription()).thenReturn("Description");
+
+        assertEquals(HttpStatus.CREATED, challengeController.createChallenge(challenge, new Random().nextLong()).getStatusCode());
     }
 
     @Test
     public void testReadChallenge_Should_Return_Status_Code_200() throws Exception {
-        assertEquals(HttpStatus.OK, challengeController.readChallenge(1L, mockedUser.getId()).getStatusCode());
-        when(mockedChallenge.getChallengeClaimed()).thenReturn(true);
+        assertEquals(HttpStatus.OK, challengeController.readChallenge(1L, user.getId()).getStatusCode());
+
+        when(challenge.getChallengeClaimed()).thenReturn(true);
     }
 
     @Test
     public void testReadChallenge_Should_Return_Status_Code_400() throws Exception {
-        when(mockedUser.getId()).thenReturn(1L);
-        when(mockedUser.getId()).thenReturn(2L);
-        when(mockedChallenge.getChallengeCompleted()).thenReturn(false);
-        when(mockedChallenge.getYoutubeVideoUploaded()).thenReturn(false);
-        when(mockedChallenge.getChallengeClaimed()).thenReturn(true);
-        assertEquals(HttpStatus.BAD_REQUEST, challengeController.readChallenge(1L, mockedUser.getId()).getStatusCode());
+        when(user.getId()).thenReturn(1L);
+        when(user.getId()).thenReturn(2L);
+        when(challenge.getChallengeCompleted()).thenReturn(false);
+        when(challenge.getYoutubeVideoUploaded()).thenReturn(false);
+        when(challenge.getChallengeClaimed()).thenReturn(true);
+
+        assertEquals(HttpStatus.BAD_REQUEST, challengeController.readChallenge(1L, user.getId()).getStatusCode());
     }
 
     @Test
@@ -96,11 +98,12 @@ public class ChallengeControllerTest {
 
     @Test
     public void testUpdateChallengeClaimer() throws Exception {
-        when(mockedUser.getId()).thenReturn(1L);
-        assertEquals(HttpStatus.OK, challengeController.updateChallengeClaimer(1L, mockedUser).getStatusCode());
+        when(user.getId()).thenReturn(1L);
+        assertEquals(HttpStatus.OK, challengeController.updateChallengeClaimer(1L, user).getStatusCode());
         // Set the two mocked users to the same id, should return a bad request http status.
-        when(mockedChallenge.getChallengeCreator().getId()).thenReturn(1L);
-        assertEquals(HttpStatus.BAD_REQUEST, challengeController.updateChallengeClaimer(1L, mockedUser).getStatusCode());
+
+        when(challenge.getChallengeCreator().getId()).thenReturn(1L);
+        assertEquals(HttpStatus.BAD_REQUEST, challengeController.updateChallengeClaimer(1L, user).getStatusCode());
     }
 
     @Test
@@ -110,7 +113,7 @@ public class ChallengeControllerTest {
 
     @Test
     public void testAssignPointsToUser_Should_Return_Status_Code_200() throws Exception {
-        when(mockedUserService.getUserFromDatabase(mockedChallenge.getChallengeCreator().getId())).thenReturn(mockedUser);
+        when(userService.getUserFromDatabase(challenge.getChallengeCreator().getId())).thenReturn(user);
         assertEquals(HttpStatus.OK, challengeController.assignPointsToUser(1L).getStatusCode());
     }
 
@@ -122,23 +125,25 @@ public class ChallengeControllerTest {
     @Test
     public void testConfirmUploadedYoutubeUrl() throws Exception {
         assertEquals(HttpStatus.OK, challengeController.confirmUploadedYoutubeUrl(1L).getStatusCode());
-        when(mockedChallenge.getYoutubeVideoUploaded()).thenReturn(true);
+        when(challenge.getYoutubeVideoUploaded()).thenReturn(true);
+
         assertEquals(HttpStatus.BAD_REQUEST, challengeController.confirmUploadedYoutubeUrl(1L).getStatusCode());
     }
 
     @Test
     public void testAddOrRemovePointToCompletedChallenge_Should_Return_Status_Code_200() throws Exception {
-        when(mockedUserService.getUserFromDatabase(mockedUser.getId())).thenReturn(mockedUser);
-        when(mockedChallengeService.getChallengeFromDatabase(1L)).thenReturn(mockedChallenge);
-        assertEquals(HttpStatus.OK, challengeController.addOrRemoveUserToChallengeUpvoters(1L, mockedUser).getStatusCode());
+        when(userService.getUserFromDatabase(user.getId())).thenReturn(user);
+        when(challengeService.getChallengeFromDatabase(1L)).thenReturn(challenge);
+
+        assertEquals(HttpStatus.OK, challengeController.addOrRemoveUserToChallengeUpvoters(1L, user).getStatusCode());
     }
 
     @Test()
     public void testAddOrRemoveUserToChallengeUpvoters_Should_Return_Status_Code_200() throws Exception {
-        when(mockedUser.getId()).thenReturn(1L);
-        when(mockedUserService.getUserFromDatabase(1L)).thenReturn(new User());
-        /*assertEquals("addUserToChallengeUpvoters() did not respond with http status 200 (ok)", HttpStatus.OK, challengeController.addOrRemoveUserToChallengeUpvoters(1L, mockedUser).getStatusCode());*/
-        assertEquals(HttpStatus.OK, challengeController.addOrRemovePointToCompletedChallenge(1L, mockedUser).getStatusCode());
+        when(user.getId()).thenReturn(1L);
+        when(userService.getUserFromDatabase(1L)).thenReturn(new User());
+
+        assertEquals(HttpStatus.OK, challengeController.addOrRemovePointToCompletedChallenge(1L, user).getStatusCode());
     }
 
 }
--- src/test/com/challengeaccepted/controllers/UserControllerTest.java
+++ src/test/com/challengeaccepted/controllers/UserControllerTest.java
@@ -4,9 +4,6 @@ import com.challengeaccepted.models.User;
 import com.challengeaccepted.services.UserService;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
